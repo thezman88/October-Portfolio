16ccf91ca5bd357daad78fb0472ef4b7c8380b91
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-09-26 16:51:49 -0400

    api is working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,14 @@
-console.log('working');
+var APIKey = "1"
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/"+ APIKey+"/search.php?s=margarita" ;
+    // api.openweathermap.org/data/2.5/weather?q={city name}
+
+    // We then created an AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+
+});
